97c0c177a312e9320dd618bc4afa1288a2607ecb
Pasang24 <pasangtheeng14@gmail.com>
2024-10-07 22:13:55 +0545

    Added initial page view for subject route and firebase storage

--- src/app/semester/[sem]/[subject]/page.js
+++ src/app/semester/[sem]/[subject]/page.js
@@ -1,6 +1,44 @@
-function page({ params }) {
-  console.log(params);
-  return <div>page</div>;
+import Container from "@/components/custom/Container";
+import { db } from "@/app/firebaseConfig";
+import { buttonVariants } from "@/components/ui/button";
+import { doc, getDoc } from "firebase/firestore";
+import { notFound } from "next/navigation";
+import { BookCopy } from "lucide-react";
+
+async function page({ params }) {
+  const subSnap = await getDoc(doc(db, "subjects", params.subject));
+  if (!subSnap.exists()) return notFound();
+
+  const subData = subSnap.data();
+
+  return (
+    <div className="flex justify-center">
+      <Container>
+        <div className="flex flex-col gap-3">
+          <h2 className="font-bold text-xl">
+            {subData.title} ({subData.code})
+          </h2>
+          <div>
+            <h3 className="font-semibold mb-1">Course Description:</h3>
+            <p className="text-sm">{subData.desc}</p>
+          </div>
+          <div>
+            <h3 className="font-semibold mb-1">Course Objective:</h3>
+            <p className="text-sm">{subData.obj}</p>
+          </div>
+          <button
+            className={`${buttonVariants({
+              variant: "outline",
+            })} self-start flex gap-2 items-center`}
+          >
+            <BookCopy strokeWidth={1} />
+            <span>View full syllabus</span>
+          </button>
+          <hr className="mt-2" />
+        </div>
+      </Container>
+    </div>
+  );
 }
 
 export default page;
--- src/components/custom/BoxContainer.js
+++ src/components/custom/BoxContainer.js
@@ -1,6 +1,6 @@
 function BoxContainer({ children }) {
   return (
-    <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-4">{children}</div>
+    <div className="grid gap-3 sm:grid-cols-2 lg:grid-cols-4">{children}</div>
   );
 }
 
