b1721628bb9285de522cbc87688e39a0ea67fe54
Pasang24 <pasangtheeng14@gmail.com>
2024-10-20 11:53:56 +0545

    Added extra details in questionContainer for questions view

--- src/app/semester/[sem]/[subject]/oldquestions/[year]/page.js
+++ src/app/semester/[sem]/[subject]/oldquestions/[year]/page.js
@@ -6,6 +6,8 @@ import {
   where,
   orderBy,
   getDocs,
+  getDoc,
+  doc,
 } from "firebase/firestore";
 import QuestionContainer from "@/components/custom/QuestionContainer";
 import Container from "@/components/custom/Container";
@@ -26,13 +28,19 @@ async function page({ params }) {
     and(where("subjectId", "==", params.subject), where("qNum", "==", 1)),
     orderBy("year")
   );
+  const subjectRef = doc(db, "subjects", params.subject);
 
-  const questionsSnapshot = await getDocs(questionsQuery);
-  const questionsYearSnapshot = await getDocs(questionsYearQuery);
+  const [questionsSnapshot, questionsYearSnapshot, subjectSnapShot] =
+    await Promise.all([
+      await getDocs(questionsQuery),
+      await getDocs(questionsYearQuery),
+      await getDoc(subjectRef),
+    ]);
 
   // if questions of selected year doesn't exist then return to Not Found page
   if (questionsSnapshot.empty) return notFound();
 
+  let subject = subjectSnapShot.data();
   let questions = [];
   let questionsYear = [];
 
@@ -50,7 +58,11 @@ async function page({ params }) {
       <Container className="flex gap-4 relative">
         <QuestionMenu years={questionsYear} currentYear={params.year} />
         <div className="flex-1 md:ml-48">
-          <QuestionContainer questions={questions} year={params.year} />
+          <QuestionContainer
+            subject={subject}
+            questions={questions}
+            year={params.year}
+          />
         </div>
       </Container>
     </div>
--- src/components/custom/QuestionContainer.js
+++ src/components/custom/QuestionContainer.js
@@ -4,7 +4,7 @@ import { MathJaxContext, MathJax } from "better-react-mathjax";
 import { convertToWords } from "react-number-to-words";
 import parse from "html-react-parser";
 
-function QuestionContainer({ questions, year }) {
+function QuestionContainer({ subject, questions, year }) {
   const longQuestions = questions.filter(
     (question) => question.type === "long"
   );
@@ -12,8 +12,16 @@ function QuestionContainer({ questions, year }) {
     (question) => question.type === "short"
   );
 
-  const attemptLong = convertToWords(longQuestions.length - 1);
-  const attemptShort = convertToWords(shortQuestions.length - 1);
+  const totalLongAttempt = longQuestions.length - 1;
+  const totalShortAttempt = shortQuestions.length - 1;
+
+  const attemptLong = convertToWords(totalLongAttempt);
+  const attemptShort = convertToWords(totalShortAttempt);
+
+  // each long question is 10 marks and short is 5 marks
+  const fullMarks = totalLongAttempt * 10 + totalShortAttempt * 5;
+  // pass marks is 40% of fullMarks;
+  const passMarks = 0.4 * fullMarks;
   return (
     <MathJaxContext
       version={2}
@@ -26,7 +34,27 @@ function QuestionContainer({ questions, year }) {
           <h3 className="text-center">Institute of Science and Technology</h3>
           <h3 className="text-center">{year}</h3>
         </div>
-
+        <div className="flex justify-between text-xs sm:text-sm">
+          <div className="flex flex-col gap-2">
+            <span className="capitalize">
+              Bachelor Level/ {subject.semester} Semester/ Science
+            </span>
+            <span>B.Sc.CSIT</span>
+            <span>{subject?.title}</span>
+          </div>
+          <div className="flex flex-col gap-2">
+            <span>Full Marks: {fullMarks}</span>
+            <span>Pass Marks: {passMarks}</span>
+            <span>Time: 3 hours</span>
+          </div>
+        </div>
+        <div className="flex flex-col gap-2 text-xs sm:text-sm font-semibold mt-6">
+          <span className="italic">
+            Candidates are required to give their answer in their own words as
+            far as practicable.
+          </span>
+          <span>The figures in the margin indicate full marks.</span>
+        </div>
         <div className="grid gap-6 sm:gap-7 mt-6">
           <h3 className="text-center underline font-semibold">Section A</h3>
           <h3 className="font-semibold">
