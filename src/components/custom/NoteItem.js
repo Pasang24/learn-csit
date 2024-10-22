bfbd1a25cb30715b1d82c1f3caf750757733f874
Pasang24 <pasangtheeng14@gmail.com>
2024-10-22 10:38:17 +0545

    Added transition to image loading in notes

---
+++ src/components/custom/NoteItem.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+import Image from "next/image";
+import { Skeleton } from "@/components/ui/skeleton";
+
+function NoteItem({ note, onClick, noteNum }) {
+  const [showImage, setShowImage] = useState(false);
+
+  useEffect(() => {
+    setShowImage(false);
+  }, [note]);
+
+  return (
+    <button
+      onClick={onClick}
+      className="bg-accent flex justify-center items-center aspect-square overflow-hidden relative"
+    >
+      <Skeleton
+        className={`w-full h-full absolute ${showImage ? "opacity-0" : ""}`}
+      />
+
+      <Image
+        className={`w-full h-full object-cover cursor-pointer opacity-0 hover:scale-110 select-none ${
+          showImage ? "opacity-100" : ""
+        }`}
+        style={{ transition: "0.3s all ease" }}
+        width={500}
+        height={500}
+        src={note}
+        alt={`Page ${noteNum}`}
+        draggable={false}
+        onLoad={() => setShowImage(true)}
+      />
+    </button>
+  );
+}
+
+export default NoteItem;
--- src/components/custom/NotesList.js
+++ src/components/custom/NotesList.js
@@ -1,9 +1,9 @@
 "use client";
 
-import Image from "next/image";
 import useModal from "@/hooks/useModal";
 import { useState } from "react";
 import ImageViewer from "./ImageViewer";
+import NoteItem from "./NoteItem";
 
 function NotesList({ notes, notesDownload }) {
   const { isOpen, openModal, closeModal } = useModal();
@@ -12,24 +12,15 @@ function NotesList({ notes, notesDownload }) {
     <>
       <div className="grid grid-cols-3 vs:grid-cols-4 lg:grid-cols-5 gap-2">
         {notes.map((note, index) => (
-          <div
+          <NoteItem
+            note={note}
             onClick={() => {
               setIndex(index);
               openModal();
             }}
-            className="bg-accent flex justify-center items-center aspect-square overflow-hidden"
-            key={index}
-          >
-            <Image
-              className="w-full h-full object-cover cursor-pointer hover:scale-110"
-              style={{ transition: "0.3s all ease" }}
-              width={500}
-              height={500}
-              src={note}
-              alt={`Page ${index + 1}`}
-              key={index}
-            />
-          </div>
+            noteNum={index + 1}
+            key={note}
+          />
         ))}
       </div>
       <ImageViewer
