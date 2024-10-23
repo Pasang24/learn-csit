815807097b34a1f25bb55d565c81fa1c4e009282
Pasang24 <pasangtheeng14@gmail.com>
2024-10-23 18:30:35 +0545

    Added drawer for showing answers of questions in mobile view

--- src/components/custom/QuestionItem.js
+++ src/components/custom/QuestionItem.js
@@ -2,51 +2,118 @@ import {
   Dialog,
   DialogContent,
   DialogDescription,
-  DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
 import { MathJax } from "better-react-mathjax";
+import { useMediaQuery } from "usehooks-ts";
+import useModal from "@/hooks/useModal";
 import parse from "html-react-parser";
 import Confused from "../illustration/Confused";
 
 export function QuestionItem({ question }) {
+  const { isOpen, openModal, closeModal } = useModal();
+  const isDesktopOrLaptop = useMediaQuery("(min-width: 768px)", {
+    initializeWithValue: false,
+  });
+
+  if (isDesktopOrLaptop) {
+    return (
+      <Dialog
+        open={isOpen}
+        onOpenChange={() => {
+          if (isOpen) {
+            closeModal();
+            window.history.back();
+          } else {
+            openModal();
+          }
+        }}
+      >
+        <DialogTrigger asChild>
+          <button className="w-fit flex items-start gap-1 vs:gap-3 hover:text-slate-300 text-left">
+            <div className="font-semibold">{question.qNum}.</div>
+            <MathJax hideUntilTypeset={"first"} inline dynamic>
+              <div>{parse(question.title)}</div>
+            </MathJax>
+          </button>
+        </DialogTrigger>
+        <DialogContent
+          className="w-11/12 max-w-3xl h-fit max-h-[calc(100vh-100px)] overflow-y-auto"
+          id="customScrollBar"
+        >
+          <DialogHeader>
+            <DialogTitle className="flex items-start gap-3 my-4 leading-7">
+              <div>{question.qNum}.</div>
+              <MathJax hideUntilTypeset={"first"} inline dynamic>
+                <div>{parse(question.title)}</div>
+              </MathJax>
+            </DialogTitle>
+            <DialogDescription className="text-base">Answer:</DialogDescription>
+          </DialogHeader>
+          <div className="flex justify-center mb-3">
+            <div className="w-96">
+              <Confused />
+              <h3 className="text-center text-2xl font-semibold">
+                Answer Unavailable :(
+              </h3>
+            </div>
+          </div>
+        </DialogContent>
+      </Dialog>
+    );
+  }
+
   return (
-    <Dialog>
-      <DialogTrigger asChild>
+    <Drawer
+      open={isOpen}
+      onOpenChange={() => {
+        if (isOpen) {
+          closeModal();
+          window.history.back();
+        } else {
+          openModal();
+        }
+      }}
+    >
+      <DrawerTrigger asChild>
         <button className="w-fit flex items-start gap-1 vs:gap-3 hover:text-slate-300 text-left">
           <div className="font-semibold">{question.qNum}.</div>
           <MathJax hideUntilTypeset={"first"} inline dynamic>
             <div>{parse(question.title)}</div>
           </MathJax>
         </button>
-      </DialogTrigger>
-      <DialogContent
-        className="max-w-3xl h-fit max-h-[calc(100vh-100px)] overflow-y-auto"
-        id="customScrollBar"
-      >
-        <DialogHeader>
-          <DialogTitle className="flex items-start gap-3 my-4 leading-7">
+      </DrawerTrigger>
+      <DrawerContent>
+        <DrawerHeader className="text-left">
+          <DrawerTitle className="flex items-start gap-3 my-4 leading-7">
             <div>{question.qNum}.</div>
             <MathJax hideUntilTypeset={"first"} inline dynamic>
               <div>{parse(question.title)}</div>
             </MathJax>
-          </DialogTitle>
-          <DialogDescription className="text-base">Answer:</DialogDescription>
-        </DialogHeader>
-        <div className="flex justify-center mb-3">
-          <div className="w-96">
+          </DrawerTitle>
+          <DrawerDescription className="text-base">Answer:</DrawerDescription>
+        </DrawerHeader>
+        <div className="flex justify-center mb-4">
+          <div className="max-w-96">
             <Confused />
-            <h3 className="text-center text-2xl font-semibold">
+            <h3 className="text-center text-xl font-semibold">
               Answer Unavailable :(
             </h3>
           </div>
         </div>
-        {/* <DialogFooter>
-            <button type="submit">Save changes</button>
-          </DialogFooter> */}
-      </DialogContent>
-    </Dialog>
+      </DrawerContent>
+    </Drawer>
   );
 }
